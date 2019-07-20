eb1ed86559a623487f0397c0cafc9e328c4769e2
Paul Fauchon <p.fauchon@gmail.com>
2019-07-20 11:51:56 +0800

    remove cache for docker build

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ docker build --no-cache --tag gonzague/trsl8-build:latest --tag gonzague/trsl8-b
 if [ $? -ne 0 ]; then   exit 1; fi
 
 #Compressing
-docker run -v "${PWD}:/go/src/github.com/lamarios/trsl8" gonzague/trsl8-build
+docker run  gonzague/trsl8-build
 if [ $? -ne 0 ]; then   exit 1; fi
 #tar -czf trsl8-$VERSION-$OS-$ARCH.tar.gz trsl8
 
