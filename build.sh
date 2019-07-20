d3bd28cc767fffe4b92d77d6de519f257af31610
Paul Fauchon <p.fauchon@gmail.com>
2019-07-20 13:04:48 +0800

    remove cache for docker build

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ if [ $? -ne 0 ]; then   exit 1; fi
 
 docker cp ${BUILD_NAME}:/go/src/github.com/lamarios/trsl8/trsl8 ./trsl8
 if [ $? -ne 0 ]; then   exit 1; fi
-ls
+ls -lh
 #tar -czf trsl8-$VERSION-$OS-$ARCH.tar.gz trsl8
 
 echo "Building the Web"
@@ -41,6 +41,7 @@ cp trsl8 docker/trsl8
 cp -R static docker/static
 
 cd docker
+ls -lh
 docker build --no-cache --tag gonzague/trsl8:latest --tag gonzague/trsl8:${VERSION} .
 if [ $? -ne 0 ]; then   exit 1; fi
 cd -
